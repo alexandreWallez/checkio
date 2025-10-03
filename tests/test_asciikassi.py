1f675de5eca1cbe5b320bf1b3b2664acf18dfc8f
alexandre.wallez <alexandre.wallez.etu@univ-lille.fr>
2025-10-03 12:09:44 +0200

    update

--- tests/test_asciikassi.py
+++ tests/test_asciikassi.py
@@ -1,6 +1,6 @@
 from checkio import asciikassi
 
 def test_asciikassi() :
-    assert asciikassi(0) == "++\n++"
-    assert asciikassi(1) == "+-+\n| |\n+-+"
-    assert asciikassi(2) == "+--+\n|  |\n|  |\n+--+"
+    assert asciikassi.asciikassi(0) == "++\n++"
+    assert asciikassi.asciikassi(1) == "+-+\n| |\n+-+"
+    assert asciikassi.asciikassi(2) == "+--+\n|  |\n|  |\n+--+"
